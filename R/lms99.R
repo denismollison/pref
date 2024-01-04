765b8554eb55b41ac1228785b4ac596d3f3205eb
Denis Mollison <denis.mollison@gmail.com>
2024-01-04 14:45:49 +0000

    tidying up files in limbo

---
+++ R/lms99.R
@@ -0,0 +1,11 @@
+#' Example of an STV election allowing equal preferences - London Mathematical Society 1999
+#'
+#' Votes are in a ballot-format matrix
+#'
+#' @format A list with 10 variables, largest the vote matrix (343 x 10)
+#' \describe{q()
+#'   \item{s}{number of seats}
+#'   \item{c}{number of candidates}
+#'   ...
+#' }
+"lms99"
---
+++ R/stv_result.R
@@ -0,0 +1,31 @@
+# stv.result - text of main statistics of election count
+
+#' Prints main details of result of an STV election
+#'
+#' @param outlist An R list of results from stv or stv.wig
+#' @return Prints out results
+#' @export
+#'
+#' @examples lms99result=stv(lms99)
+#' @examples stv.result(lms99result)
+#' @examples nws17wig=stv.wig(nws17)
+#' @examples stv.result(nws17wig)
+
+stv.result=function(outlist){
+sys=outlist$sys
+ct=outlist$counttext; nst=dim(ct)[[2]]
+options(width=max(c(120,nchar(ct))))
+cat("\nElection: ",outlist$elecname," (",outlist$sys," STV)\n\n",sep="")
+# count text:
+for(i in 1:nst){
+cat(ct[1,i]);cat("\n")
+cat("  ",ct[2,i]); cat("\n\n")
+}
+cat("Those elected, in order of election:\n")
+cat(outlist$elec); cat("\n\n")
+cat("Votes at each stage")
+if(sys=="meek"){cat(" and final keep values:")}
+cat("\n")
+print(round(outlist$votes,2)); cat("\n")
+cat(outlist$quotatext)
+}
